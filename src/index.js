38f9b082df37a672d817c91ed70fa90296c4b62e
Wesley-Oliveira <wesley0leby@gmail.com>
2021-05-04 15:29:26 -0300

    Using HTTP methods, type params, adding nodemon on application and work with insomnia

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express =  require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+})
+
+app.post("/courses", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+})
+
+app.put("/courses/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+})
+
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+})
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+    ]);
+})
+
+app.listen(3333);
